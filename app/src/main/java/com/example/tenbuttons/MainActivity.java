018b6d2c7a32e942b0f446c6141f05ec86cb3e98
leofab <leofabr3@gmail.com>
2023-02-18 18:40:38 -0300

    secondviewimplement

--- app/src/main/java/com/example/tenbuttons/MainActivity.java
+++ app/src/main/java/com/example/tenbuttons/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tenbuttons;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         Button button1 = findViewById(R.id.button1);
+
+        button1.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        });
+
         Button button2 = findViewById(R.id.button2);
         Button button3 = findViewById(R.id.button3);
         Button button4 = findViewById(R.id.button4);
