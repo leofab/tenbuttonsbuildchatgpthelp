ecb7ca3523b063478fe118a2aec41cf56e18e4a7
leofab <leofabr3@gmail.com>
2023-02-18 18:55:32 -0300

    second view not working properly

--- app/src/main/java/com/example/tenbuttons/MainActivity.java
+++ app/src/main/java/com/example/tenbuttons/MainActivity.java
@@ -1,13 +1,11 @@
 package com.example.tenbuttons;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,11 +16,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button button1 = findViewById(R.id.button1);
 
-        button1.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-            startActivity(intent);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
         });
-
         Button button2 = findViewById(R.id.button2);
         Button button3 = findViewById(R.id.button3);
         Button button4 = findViewById(R.id.button4);
@@ -45,41 +45,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button10.setOnClickListener(this);
     }
 
-    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View view) {
-        // Handle button click events
-        switch (view.getId()) {
-            case R.id.button1:
-                Toast.makeText(this, "Button 1 clicked", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.button2:
-                Toast.makeText(this, "Button 2 clicked", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.button3:
-                Toast.makeText(this, "Button 3 clicked", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.button4:
-                Toast.makeText(this, "Button 4 clicked", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.button5:
-                Toast.makeText(this, "Button 5 clicked", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.button6:
-                Toast.makeText(this, "Button 6 clicked", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.button7:
-                Toast.makeText(this, "Button 7 clicked", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.button8:
-                Toast.makeText(this, "Button 8 clicked", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.button9:
-                Toast.makeText(this, "Button 9 clicked", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.button10:
-                Toast.makeText(this, "Button 10 clicked", Toast.LENGTH_SHORT).show();
-                break;
-        }
+
     }
+
+//    @SuppressLint("NonConstantResourceId")
+//    @Override
+//    public void onClick(View view) {
+//        // Handle button click events
+//        switch (view.getId()) {
+//            case R.id.button1:
+//                Toast.makeText(this, "Button 1 clicked", Toast.LENGTH_SHORT).show();
+//                break;
+//            case R.id.button2:
+//                Toast.makeText(this, "Button 2 clicked", Toast.LENGTH_SHORT).show();
+//                break;
+//            case R.id.button3:
+//                Toast.makeText(this, "Button 3 clicked", Toast.LENGTH_SHORT).show();
+//                break;
+//            case R.id.button4:
+//                Toast.makeText(this, "Button 4 clicked", Toast.LENGTH_SHORT).show();
+//                break;
+//            case R.id.button5:
+//                Toast.makeText(this, "Button 5 clicked", Toast.LENGTH_SHORT).show();
+//                break;
+//            case R.id.button6:
+//                Toast.makeText(this, "Button 6 clicked", Toast.LENGTH_SHORT).show();
+//                break;
+//            case R.id.button7:
+//                Toast.makeText(this, "Button 7 clicked", Toast.LENGTH_SHORT).show();
+//                break;
+//            case R.id.button8:
+//                Toast.makeText(this, "Button 8 clicked", Toast.LENGTH_SHORT).show();
+//                break;
+//            case R.id.button9:
+//                Toast.makeText(this, "Button 9 clicked", Toast.LENGTH_SHORT).show();
+//                break;
+//            case R.id.button10:
+//                Toast.makeText(this, "Button 10 clicked", Toast.LENGTH_SHORT).show();
+//                break;
+//        }
+//    }
 }
