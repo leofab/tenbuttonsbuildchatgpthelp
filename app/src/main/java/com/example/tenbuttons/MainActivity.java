cc940b8a9a14f426ae5585f8cc66957f8244ba8a
leofab <leofabr3@gmail.com>
2023-02-18 19:43:48 -0300

    refactor TOAST messages

--- app/src/main/java/com/example/tenbuttons/MainActivity.java
+++ app/src/main/java/com/example/tenbuttons/MainActivity.java
@@ -46,42 +46,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
 
     }
-
-//    @SuppressLint("NonConstantResourceId")
-//    @Override
-//    public void onClick(View view) {
-//        // Handle button click events
-//        switch (view.getId()) {
-//            case R.id.button1:
-//                Toast.makeText(this, "Button 1 clicked", Toast.LENGTH_SHORT).show();
-//                break;
-//            case R.id.button2:
-//                Toast.makeText(this, "Button 2 clicked", Toast.LENGTH_SHORT).show();
-//                break;
-//            case R.id.button3:
-//                Toast.makeText(this, "Button 3 clicked", Toast.LENGTH_SHORT).show();
-//                break;
-//            case R.id.button4:
-//                Toast.makeText(this, "Button 4 clicked", Toast.LENGTH_SHORT).show();
-//                break;
-//            case R.id.button5:
-//                Toast.makeText(this, "Button 5 clicked", Toast.LENGTH_SHORT).show();
-//                break;
-//            case R.id.button6:
-//                Toast.makeText(this, "Button 6 clicked", Toast.LENGTH_SHORT).show();
-//                break;
-//            case R.id.button7:
-//                Toast.makeText(this, "Button 7 clicked", Toast.LENGTH_SHORT).show();
-//                break;
-//            case R.id.button8:
-//                Toast.makeText(this, "Button 8 clicked", Toast.LENGTH_SHORT).show();
-//                break;
-//            case R.id.button9:
-//                Toast.makeText(this, "Button 9 clicked", Toast.LENGTH_SHORT).show();
-//                break;
-//            case R.id.button10:
-//                Toast.makeText(this, "Button 10 clicked", Toast.LENGTH_SHORT).show();
-//                break;
-//        }
-//    }
 }
